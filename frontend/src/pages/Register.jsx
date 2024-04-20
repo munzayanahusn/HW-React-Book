8e0711680de5003e96a5bec35509f2fa84c33b99
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-04-20 14:48:59 +0700

    style: registration

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -24,73 +24,84 @@ const Register = () => {
     }
   };
 
+  const handleCancel = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="w-full py-4 px-24 mx-auto mt-8">
-      <h1 className="text-xl font-bold mb-4">Register</h1>
+    <div className="w-full py-4 px-24 mx-auto mt-8 style={{ width: '2000px' }}">
+      <h1 className="text-3xl text-slate-900 font-bold mb-4">Account Registration</h1>
 
       <div className="border-2 border-gray-200 rounded-lg p-4">
-        <form onSubmit={handleSubmit}>
-          {error && <div className="text-red-500 mb-4">{error}</div>}
+      <form onSubmit={handleSubmit}>
+        {error && <div className="text-red-500 mb-4">{error}</div>}
 
-          <div className="mb-4">
-            <label htmlFor="name" className="block">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              className="border border-gray-300 rounded p-2 w-full"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
+        <div className="flex mb-4 items-center">
+          <label htmlFor="name" className="block text-slate-700 text-left mr-4" style={{ width: '150px' }}>Name</label>
+          <p className="text-slate-700 mr-2">:</p>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+            className="bg-white border border-gray-300 rounded p-2 w-full"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
 
-          <div className="mb-4">
-            <label htmlFor="email" className="block">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email address"
-              className="border border-gray-300 rounded p-2 w-full"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
+        <div className="flex mb-4 items-center">
+          <label htmlFor="email" className="block text-slate-700 text-left mr-4" style={{ width: '150px' }}>Email</label>
+          <p className="text-slate-700 mr-2">:</p>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email address"
+            className="bg-white border border-gray-300 rounded p-2 w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
 
-          <div className="mb-4">
-            <label htmlFor="password" className="block">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter a password"
-              className="border border-gray-300 rounded p-2 w-full"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+        <div className="flex mb-4 items-center">
+          <label htmlFor="password" className="block text-slate-700 text-left mr-4" style={{ width: '150px' }}>Password</label>
+          <p className="text-slate-700 mr-2">:</p>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter a password"
+            className="bg-white border border-gray-300 rounded p-2 w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
 
-          <div className="mb-4">
-            <label htmlFor="confirmPassword" className="block">Confirm Password</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              placeholder="Confirm your password"
-              className={`border border-gray-300 rounded p-2 w-full ${password !== confirmPassword && 'border-red-500'}`}
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-            {password !== confirmPassword && (
-              <p className="text-xs text-red-500">The password does not match</p>
-            )}
-          </div>
+        <div className="flex mb-4 items-center">
+          <label htmlFor="confirmPassword" className="block text-left text-slate-700 mr-4" style={{ width: '150px' }}>Confirm Password</label>
+          <p className="text-slate-700 mr-2">:</p>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Confirm your password"
+            className={`bg-white border border-gray-300 rounded p-2 w-full ${password !== confirmPassword && 'border-red-500'}`}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+          {password !== confirmPassword && (
+            <p className="text-xs text-red-500">The password does not match</p>
+          )}
+        </div>
 
-          <button className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded" type="submit">Register</button>
-        </form>
+        <div className="flex justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 mr-5 rounded" type="submit">Register</button>
+          <button className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 rounded" onClick={handleCancel}>Cancel</button>
+        </div>
+      </form>
       </div>
     </div>
   );
